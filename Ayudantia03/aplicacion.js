bcb1b31bffd2f382c48024cbdec274a22146a88e
rstuardoc <rstuardoc@uc.cl>
2021-04-29 17:05:44 -0400

    tarea6

---
+++ Ayudantia03/aplicacion.js
@@ -0,0 +1,51 @@
+$(document).ready(function(){
+    var divs = $(".divs");
+    var links= $(".links");
+    var titulo= $("#titulo")
+    var boton = $("#boton") 
+    var boton2 = $("#boton2")
+    var sub= $("#subtitulo")
+
+    titulo.mouseenter(function(){
+        $(this).fadeTo("fast",0.5);
+    
+    });
+
+    boton.click(function(){
+        divs.hide()
+
+    });
+
+
+    divs.mouseenter(function(){
+      $(this).css("background", "black");
+
+    });
+
+
+    boton2.click(function(){
+        sub.text("Los perritos más hermosos del mundo");
+
+
+    
+
+    });
+
+
+
+
+
+
+
+    });
+
+
+
+
+
+
+
+
+
+
+
